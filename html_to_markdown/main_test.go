720dd4cd5f1df5e9c69a09897987027bf41217e5
Dmitri Shuralyov <shurcooL@gmail.com>
2014-02-20 21:16:24 -0500

    Add WIP html_to_markdown package.
    It works for simple cases, but the spacing may be off for less standard
    html input. Needs to properly support multiple paragraphs.

---
+++ html_to_markdown/main_test.go
@@ -0,0 +1,48 @@
+package html_to_markdown_test
+
+import (
+	"bytes"
+	"testing"
+
+	"code.google.com/p/go.net/html"
+	"github.com/russross/blackfriday"
+	"github.com/shurcooL/go-goon"
+	"github.com/shurcooL/go/html_to_markdown"
+)
+
+func Test1(t *testing.T) {
+	const markdownIn = `This is Markdown.
+
+- Item
+- Item
+- Item
+
+And here are some ordered items.
+
+1. Item 1
+1. Item 2
+1. Item 3
+
+Cool.
+`
+
+	renderedHtml := blackfriday.MarkdownBasic([]byte(markdownIn))
+
+	parsedHtml, err := html.Parse(bytes.NewReader(renderedHtml))
+	if err != nil {
+		panic(err)
+	}
+
+	markdownOut := html_to_markdown.Unnamed1(parsedHtml)
+
+	if markdownIn != markdownOut {
+		goon.DumpExpr(markdownIn, markdownOut)
+		t.Fail()
+	}
+}
+
+/* More test material:
+
+<strong>Blah blah: </strong>blah blah blah <strong>blah blah.</strong> Blah blah.
+
+*/
