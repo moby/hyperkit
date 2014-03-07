7ba9c20b0f9abc77bfd931d5d39089210fc1dbc3
Dmitri Shuralyov <shurcooL@gmail.com>
2014-03-06 23:42:26 -0800

    Use html_to_markdown package.

--- html_table/main.go
+++ html_table/main.go
@@ -2,61 +2,9 @@ package html_table
 
 import (
 	"code.google.com/p/go.net/html"
-	"code.google.com/p/go.net/html/atom"
+
+	"github.com/shurcooL/go/html_to_markdown"
 )
-
-// clean replaces each sequence of space, \n, \r, or \t characters
-// with a single space and removes any trailing and leading spaces.
-func clean(s string) string {
-	var b []byte
-	p := byte(' ')
-	for i := 0; i < len(s); i++ {
-		q := s[i]
-		if q == '\n' || q == '\r' || q == '\t' {
-			q = ' '
-		}
-		if q != ' ' || p != ' ' {
-			b = append(b, q)
-			p = q
-		}
-	}
-	// Remove trailing blank, if any.
-	if n := len(b); n > 0 && p == ' ' {
-		b = b[0 : n-1]
-	}
-	return string(b)
-}
-
-// getAttribute returns an attribute of a node, or blank strink if not found.
-func getAttribute(n *html.Node, key string) (val string) {
-	for _, attr := range n.Attr {
-		if attr.Key == key {
-			return attr.Val
-		}
-	}
-	return ""
-}
-
-// extract returns the recursive concatenation of the raw text contents of an html node, with Markdown tags.
-func extract(n *html.Node) (out string) {
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		if c.Type == html.TextNode {
-			out += c.Data
-		} else if c.Type == html.ElementNode && c.DataAtom == atom.A {
-			out += "[" + extract(c) + "](" + getAttribute(c, "href") + ")"
-		} else if c.Type == html.ElementNode && c.DataAtom == atom.B {
-			out += "**" + extract(c) + "**"
-		} else {
-			out += extract(c)
-		}
-	}
-	return out
-}
-
-// htmlNodeToMarkdown renders an html node to Markdown.
-func htmlNodeToMarkdown(htmlNode *html.Node) (markdown string) {
-	return clean(extract(htmlNode))
-}
 
 // WalkRows walks the rows of an html table, calling walkFunc on each row.
 func WalkRows(htmlTable *html.Node, walkFunc func(columns ...string)) {
@@ -68,7 +16,7 @@ func WalkRows(htmlTable *html.Node, walkFunc func(columns ...string)) {
 
 			for c := n.FirstChild; c != nil; c = c.NextSibling {
 				if c.Type == html.ElementNode && c.Data == "td" {
-					columns = append(columns, htmlNodeToMarkdown(c))
+					columns = append(columns, html_to_markdown.Paragraph(c))
 				}
 			}
 
