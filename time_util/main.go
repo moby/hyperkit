87ebc4c8d59c339bbdbf48fd4d81024fb783c149
Dmitri Shuralyov <shurcooL@gmail.com>
2014-04-19 14:11:56 -0700

    Add time_util package with func to get beginning of the week.

---
+++ time_util/main.go
@@ -0,0 +1,8 @@
+package time_util
+
+import "time"
+
+// Returns time at start of week of t, in local timezone.
+func StartOfWeek(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day()-int(t.Weekday()), 0, 0, 0, 0, time.Local)
+}
