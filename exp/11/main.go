38546b03741853136d6ecdd257c55475918126fc
Dmitri Shuralyov <shurcooL@gmail.com>
2014-04-25 00:23:07 -0700

    Work on sorting imports in InlineDotImports().
    It's really bad. Editing an existing AST is near impossible, because I
    need to be able to insert a newline, but that requires modifying the
    token.FileSet, which only has public API for merging lines, not adding
    new ones. I end up having to print AST, modify the text, and
    (optionally) parse to get expected AST back. But it works and passes a
    test for now.

--- exp/11/main.go
+++ exp/11/main.go
@@ -7,18 +7,25 @@ import (
 	"go/parser"
 	"go/token"
 	"io"
+	"os"
 	"strings"
+
 	"code.google.com/p/go.tools/go/loader"
+	"code.google.com/p/go.tools/imports"
+
 	//"code.google.com/p/go.tools/go/types"
 	//"honnef.co/go/importer"
 
 	. "gist.github.com/5286084.git"
 	. "gist.github.com/5504644.git"
 	. "gist.github.com/5639599.git"
+
+	"github.com/shurcooL/go/exp/15"
 )
 
 var _ = AstPackageFromBuildPackage
 var _ = PrintlnAst
+var _ = exp15.SortImports
 
 const parserMode = parser.ParseComments
 const astMergeMode = 0*ast.FilterFuncDuplicates | ast.FilterUnassociatedComments | ast.FilterImportDuplicates
@@ -83,29 +90,105 @@ func InlineDotImports(w io.Writer, importPath string) {
 
 	merged := ast.MergePackageFiles(apkg, astMergeMode)
 
-	fmt.Fprintln(w, "package "+SprintAst(prog.Fset, merged.Name))
-	fmt.Fprintln(w)
-	fmt.Fprintln(w, `import (`)
+	switch 3 {
+	case 1:
+		fmt.Fprintln(w, "package "+SprintAst(prog.Fset, merged.Name))
+		fmt.Fprintln(w)
+		fmt.Fprintln(w, `import (`)
+		// TODO: SortImports (ala goimports).
+		for _, importSpec := range merged.Imports {
+			if importSpec.Name != nil && importSpec.Name.Name == "." {
+				continue
+			}
+			fmt.Fprintln(w, "\t"+SprintAst(prog.Fset, importSpec))
+		}
+		fmt.Fprintln(w, `)`)
+		fmt.Fprintln(w)
+
+		for _, decl := range merged.Decls {
+			if x, ok := decl.(*ast.GenDecl); ok && x.Tok == token.IMPORT {
+				continue
+			}
+
+			fmt.Fprintln(w, SprintAst(prog.Fset, decl))
+			fmt.Fprintln(w)
+		}
+	case 2:
+		sortDecls(merged)
+
+		//fmt.Fprintln(w, SprintAst(token.NewFileSet(), merged))
+
+		//ast.SortImports(prog.Fset, merged)
+		exp15.SortImports2(prog.Fset, merged)
+
+		fmt.Fprintln(w, SprintAst(prog.Fset, merged))
+	case 3:
+		sortDecls(merged)
+
+		// TODO: Clean up this mess...
+		fset2, f2 := exp15.SortImports2(token.NewFileSet(), merged)
+
+		fmt.Fprintln(w, "package "+SprintAst(prog.Fset, merged.Name))
+		for _, decl := range f2.Decls {
+			if x, ok := decl.(*ast.GenDecl); ok && x.Tok == token.IMPORT {
+				fmt.Fprintln(w)
+				fmt.Fprintln(w, SprintAst(fset2, decl))
+			}
+		}
+		for _, decl := range merged.Decls {
+			if x, ok := decl.(*ast.GenDecl); ok && (x.Tok == token.IMPORT || x.Tok == token.PACKAGE) {
+				continue
+			}
+
+			fmt.Fprintln(w)
+			fmt.Fprintln(w, SprintAst(prog.Fset, decl))
+		}
+	case 4:
+		sortDecls(merged)
+
+		src := []byte(SprintAst(prog.Fset, merged))
+
+		out, err := imports.Process("", src, nil)
+		if err != nil {
+			panic(err)
+		}
+		os.Stdout.Write(out)
+		fmt.Println()
+	}
+}
+
+func sortDecls(merged *ast.File) {
+	var sortedDecls []ast.Decl
+	for _, decl := range merged.Decls {
+		if x, ok := decl.(*ast.GenDecl); ok && x.Tok == token.PACKAGE {
+			sortedDecls = append(sortedDecls, decl)
+		}
+	}
+	/*for _, decl := range merged.Decls {
+		if x, ok := decl.(*ast.GenDecl); ok && x.Tok == token.IMPORT {
+			sortedDecls = append(sortedDecls, decl)
+			goon.DumpExpr(decl)
+		}
+	}*/
+	var specs []ast.Spec
 	for _, importSpec := range merged.Imports {
 		if importSpec.Name != nil && importSpec.Name.Name == "." {
 			continue
 		}
-		fmt.Fprintln(w, "\t"+SprintAst(prog.Fset, importSpec))
+		importSpec.EndPos = 0
+		specs = append(specs, importSpec)
 	}
-	fmt.Fprintln(w, `)`)
-	fmt.Fprintln(w)
-
+	sortedDecls = append(sortedDecls, &ast.GenDecl{
+		Tok:    token.IMPORT,
+		Lparen: (token.Pos)(1), // Needs to be non-zero to be considered as a group.
+		Specs:  specs,
+	})
+	//goon.DumpExpr(sortedDecls[len(sortedDecls)-1])
 	for _, decl := range merged.Decls {
-		if x, ok := decl.(*ast.GenDecl); ok && x.Tok == token.IMPORT {
+		if x, ok := decl.(*ast.GenDecl); ok && (x.Tok == token.IMPORT || x.Tok == token.PACKAGE) {
 			continue
 		}
-
-		fmt.Fprintln(w, SprintAst(prog.Fset, decl))
-		fmt.Fprintln(w)
+		sortedDecls = append(sortedDecls, decl)
 	}
-
-	// TODO: Make this work equivalent to above
-	//fmt.Fprintln(w, SprintAst(imp.Fset, merged))
-
-	//goon.Dump(merged)
+	merged.Decls = sortedDecls
 }
