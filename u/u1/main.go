3e144e532dcfb02b382434aa8d1f70a79a8dbaa5
Dmitri Shuralyov <shurcooL@gmail.com>
2014-05-18 12:02:18 -0700

    Add WriteGitHubFlavoredMarkdown(), currently using github API.

--- u/u1/main.go
+++ u/u1/main.go
@@ -1,6 +1,12 @@
 package u1
 
-import "github.com/russross/blackfriday"
+import (
+	"bytes"
+	"io"
+	"net/http"
+
+	"github.com/russross/blackfriday"
+)
 
 // GitHub Flavored Markdown-like extensions.
 var MarkdownGfmExtensions = 0 |
@@ -21,4 +27,41 @@ func MarkdownGfm(input []byte) []byte {
 	renderer := blackfriday.HtmlRenderer(htmlFlags, "", "")
 
 	return blackfriday.Markdown(input, renderer, MarkdownGfmExtensions)
+}
+
+// ---
+
+// Convert GitHub Flavored Markdown to full HTML page and write to w.
+// TODO: Do this locally via a native Go library... That's not too much to ask for, is it?
+func WriteMarkdownGfmAsHtmlPage(w io.Writer, markdown []byte) {
+	// TODO: Do GitHub, fallback to local if it fails.
+	writeGitHubFlavoredMarkdownViaGitHub(w, markdown)
+	//writeGitHubFlavoredMarkdownViaLocal(w, markdown)
+}
+
+func writeGitHubFlavoredMarkdownViaLocal(w io.Writer, markdown []byte) {
+	// TODO: Don't hotlink the css file from github.com, serve it locally (it's needed for the GFM html to appear properly)
+	// TODO: Use github.com/sourcegraph/syntaxhighlight to add missing syntax highlighting.
+	io.WriteString(w, `<html><head><meta charset="utf-8"><style>code { tab-size: 4; }</style><link href="https://github.com/assets/github.css" media="all" rel="stylesheet" type="text/css" /></head><body><article class="markdown-body entry-content" style="padding: 30px;">`)
+	w.Write(MarkdownGfm(markdown))
+	io.WriteString(w, `</article></body></html>`)
+}
+
+// TODO: Remove once local version gives matching results.
+func writeGitHubFlavoredMarkdownViaGitHub(w io.Writer, markdown []byte) {
+	// TODO: Don't hotlink the css file from github.com, serve it locally (it's needed for the GFM html to appear properly)
+	io.WriteString(w, `<html><head><meta charset="utf-8"><link href="https://github.com/assets/github.css" media="all" rel="stylesheet" type="text/css" /></head><body><article class="markdown-body entry-content" style="padding: 30px;">`)
+
+	// Convert GitHub-Flavored-Markdown to HTML (includes syntax highlighting for diff, Go, etc.)
+	resp, err := http.Post("https://api.github.com/markdown/raw", "text/x-markdown", bytes.NewReader(markdown))
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	_, err = io.Copy(w, resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	io.WriteString(w, `</article></body></html>`)
 }
