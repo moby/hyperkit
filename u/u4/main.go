cb47b0003004181f18f5e0f87d2719029b61f32d
Dmitri Shuralyov <shurcooL@gmail.com>
2014-05-19 20:56:51 -0700

    u4.Open: Add some support for linux, windows.

--- u/u4/main.go
+++ u/u4/main.go
@@ -3,12 +3,23 @@ package u4
 import (
 	"log"
 	"os/exec"
+	"runtime"
 )
 
 // Open opens a file (or a directory or Url), just as if the user had double-clicked the file's icon.
 // It uses the default application, as determined by the OS.
 func Open(path string) {
-	cmd := exec.Command("open", path)
+	var args []string
+	switch runtime.GOOS {
+	case "darwin":
+		args = []string{"open"}
+	case "windows":
+		args = []string{"cmd", "/c", "start"}
+	default:
+		args = []string{"xdg-open"}
+	}
+	args = append(args, path)
+	cmd := exec.Command(args[0], args[1:]...)
 	err := cmd.Run()
 	if err != nil {
 		log.Println("u4.Open:", err)
