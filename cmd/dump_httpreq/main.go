bc910b7cde18ea3d16e7404c6cf6743f538d90af
Dmitri Shuralyov <shurcooL@gmail.com>
2014-06-06 00:50:37 -0700

    Add req.URL.Query() to dump_httpreq.

--- cmd/dump_httpreq/main.go
+++ cmd/dump_httpreq/main.go
@@ -5,17 +5,20 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httputil"
+
+	"github.com/shurcooL/go-goon"
 )
 
 var httpFlag = flag.String("http", ":8080", "Listen for HTTP connections on this address")
 
-func dumpRequestHandler(w http.ResponseWriter, r *http.Request) {
-	dump, err := httputil.DumpRequest(r, true)
+func dumpRequestHandler(w http.ResponseWriter, req *http.Request) {
+	dump, err := httputil.DumpRequest(req, true)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println(string(dump))
+	goon.DumpExpr(req.URL.Query())
 }
 
 func main() {
