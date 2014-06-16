073a25a5cb83efa6b576f7ba8fa6a23f4f380653
Dmitri Shuralyov <shurcooL@gmail.com>
2014-06-15 19:10:36 -0700

    gopath_util: Improve Remove to work on repo-level.
    Use github.com/kisielk/gotool for `go list` functionality until there's
    something more official.

--- gopath_util/main.go
+++ gopath_util/main.go
@@ -2,31 +2,56 @@ package gopath_util
 
 import (
 	"errors"
+	"strings"
 
 	. "gist.github.com/7480523.git"
 
+	"github.com/kisielk/gotool"
 	"github.com/shurcooL/go/trash"
 	"github.com/shurcooL/gostatus/status"
 )
 
-// TODO: Support pattern matching like `go list`.
-func Remove(importPath string) error {
-	goPackage := GoPackageFromImportPath(importPath)
-	if goPackage == nil {
-		return errors.New("Import Path not found: " + importPath)
+// Moves go gettable repo with no local changes into trash.
+// importPathPattern must match exactly with the repo root.
+// For example, "github.com/user/repo/...".
+func RemoveRepo(importPathPattern string) error {
+	// TODO: Use an official Go package for `go list` functionality whenever possible.
+	importPaths := gotool.ImportPaths([]string{importPathPattern})
+	if len(importPaths) == 0 {
+		return errors.New("no packages to remove")
 	}
 
-	if goPackage.Bpkg.Goroot {
-		return errors.New("can't remove packages from GOROOT")
+	var firstGoPackage *GoPackage
+	for i, importPath := range importPaths {
+		goPackage := GoPackageFromImportPath(importPath)
+		if goPackage == nil {
+			return errors.New("Import Path not found: " + importPath)
+		}
+
+		if goPackage.Bpkg.Goroot {
+			return errors.New("can't remove packages from GOROOT")
+		}
+
+		goPackage.UpdateVcs()
+
+		if goPackage.Dir.Repo == nil {
+			return errors.New("can't get repo status")
+		}
+
+		if i == 0 {
+			firstGoPackage = goPackage
+		} else if firstGoPackage.Dir.Repo != goPackage.Dir.Repo {
+			return errors.New("matched Go Packages span more than 1 repo: " + firstGoPackage.Dir.Repo.Vcs.RootPath() + " != " + goPackage.Dir.Repo.Vcs.RootPath())
+		} else if !strings.HasPrefix(goPackage.Bpkg.Dir, firstGoPackage.Dir.Repo.Vcs.RootPath()) { // TODO: This is probably not neccessary...
+			return errors.New("Go Package not inside repo: " + goPackage.Bpkg.Dir + " doesn't have prefix " + firstGoPackage.Dir.Repo.Vcs.RootPath())
+		}
 	}
 
-	goPackage.UpdateVcs()
-
-	if goPackage.Dir.Repo == nil {
-		return errors.New("can't get repo status")
+	if repoRootImportPath := strings.TrimPrefix(firstGoPackage.Dir.Repo.Vcs.RootPath(), firstGoPackage.Bpkg.SrcRoot+"/"); repoRootImportPath+"/..." != importPathPattern {
+		return errors.New("importPathPattern not exact repo root match: " + importPathPattern + " != " + repoRootImportPath + "/...")
 	}
 
-	goPackage.UpdateVcsFields()
+	firstGoPackage.UpdateVcsFields()
 
 	notableStatus := func(goPackage *GoPackage) bool {
 		// Check for notable status.
@@ -35,14 +60,10 @@ func Remove(importPath string) error {
 			packageStatus != "  +" // Updates are okay to ignore.
 	}
 
-	if notableStatus(goPackage) {
-		return errors.New("notable status: " + status.PorcelainPresenter(goPackage))
+	if notableStatus(firstGoPackage) {
+		return errors.New("notable status: " + status.PorcelainPresenter(firstGoPackage))
 	}
 
-	if goPackage.Dir.Repo.Vcs.RootPath() != goPackage.Bpkg.Dir {
-		return errors.New("repo root path mismatch: " + goPackage.Dir.Repo.Vcs.RootPath() + " != " + goPackage.Bpkg.Dir)
-	}
-
-	err := trash.MoveToTrash(goPackage.Dir.Repo.Vcs.RootPath())
+	err := trash.MoveToTrash(firstGoPackage.Dir.Repo.Vcs.RootPath())
 	return err
 }
