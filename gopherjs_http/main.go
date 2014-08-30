3136e5f64b53f67151fdb30ffc381fa35d733a81
Dmitri Shuralyov <shurcooL@gmail.com>
2014-08-30 14:36:43 -0700

    Add gopherjs_http package for serving HTML files with "text/go" scripts.
    Uses GopherJS to compile Go code into JavaScript on the fly.

---
+++ gopherjs_http/main.go
@@ -0,0 +1,131 @@
+package gopherjs_http
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+
+	"code.google.com/p/go.net/html"
+	"code.google.com/p/go.net/html/atom"
+	"github.com/shurcooL/go-goon"
+	"github.com/shurcooL/go/pipe_util"
+	"gopkg.in/pipe.v2"
+)
+
+// HtmlFile returns a handler that serves the given .html file, with the "text/go" scripts compiled to JavaScript via GopherJS.
+func HtmlFile(name string) http.Handler {
+	return &htmlFile{name: name}
+}
+
+type htmlFile struct {
+	name string
+}
+
+func (this *htmlFile) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	file, err := os.Open(this.name)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	processHtmlFile(file).WriteTo(w)
+}
+
+func processHtmlFile(r io.Reader) *bytes.Buffer {
+	var buff bytes.Buffer
+	tokenizer := html.NewTokenizer(r)
+
+	depth := 0
+
+	for {
+		if tokenizer.Next() == html.ErrorToken {
+			err := tokenizer.Err()
+			if err == io.EOF {
+				return &buff
+			}
+
+			return &bytes.Buffer{}
+		}
+
+		token := tokenizer.Token()
+		switch token.Type {
+		case html.DoctypeToken:
+			buff.WriteString(token.String())
+		case html.CommentToken:
+			buff.WriteString(token.String())
+		case html.StartTagToken:
+			if token.DataAtom == atom.Script && getType(token.Attr) == "text/go" {
+				depth++
+				//goon.Dump(token.Attr)
+				buff.WriteString(`<script type="text/javascript">`)
+			} else {
+				buff.WriteString(token.String())
+			}
+		case html.EndTagToken:
+			if token.DataAtom == atom.Script {
+				depth--
+			}
+			buff.WriteString(token.String())
+		case html.SelfClosingTagToken:
+			buff.WriteString(token.String())
+		case html.TextToken:
+			if depth > 0 {
+				buff.WriteString(goToJs(token.Data))
+			} else {
+				buff.WriteString(token.Data)
+			}
+		default:
+			panic("unknown token type")
+		}
+	}
+}
+
+func getType(attrs []html.Attribute) string {
+	for _, attr := range attrs {
+		if attr.Key == "type" {
+			return attr.Val
+		}
+	}
+	return ""
+}
+
+func goToJs(goCode string) (jsCode string) {
+	started := time.Now()
+	defer func() { fmt.Println("goToJs taken:", time.Since(started)) }()
+
+	// TODO: Don't shell out, and avoid having to write/read temporary files, instead
+	//       use http://godoc.org/github.com/gopherjs/gopherjs/compiler directly, etc.
+	p := pipe.Script(
+		pipe.Line(
+			pipe.Print(goCode),
+			pipe.WriteFile("tmp.go", 0666),
+		),
+		pipe.Exec("gopherjs", "build", "tmp.go"),
+		pipe.ReadFile("tmp.js"),
+	)
+
+	// Use a temporary dir.
+	tempDir, err := ioutil.TempDir("", "gopherjs_")
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		goon.DumpExpr(tempDir)
+		err := os.RemoveAll(tempDir)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "warning: error removing temp dir:", err)
+		}
+	}()
+
+	out, err := pipe_util.OutputDir(p, tempDir)
+	if err != nil {
+		goon.Dump(string(out), err)
+	}
+
+	return string(out)
+}
