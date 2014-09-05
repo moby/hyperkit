0f1b1cc511badacc1559793659e682730b5d4cad
Dmitri Shuralyov <shurcooL@gmail.com>
2014-09-04 20:15:40 -0700

    Stop using a dot-imported CheckError func.

--- gists/gist8065433/main.go
+++ gists/gist8065433/main.go
@@ -3,18 +3,20 @@ package gist8065433
 import (
 	"fmt"
 	"net"
-
-	. "github.com/shurcooL/go/gists/gist5286084"
 )
 
 // TODO: Better interface. Perhaps a slice of strings.
 // Get a string of non-loopback IPs.
 func Something() (out string) {
 	netInterfaces, err := net.Interfaces()
-	CheckError(err)
+	if err != nil {
+		panic(err)
+	}
 	for _, netInterface := range netInterfaces {
 		addrs, err := netInterface.Addrs()
-		CheckError(err)
+		if err != nil {
+			panic(err)
+		}
 		for _, addr := range addrs {
 			if ipnet, ok := addr.(*net.IPNet); ok {
 				if ip4 := ipnet.IP.To4(); ip4 != nil && !ip4.IsLoopback() {
