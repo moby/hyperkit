c3efaf3af89a718deded657755a96654e07c26eb
Dmitri Shuralyov <shurcooL@gmail.com>
2014-09-14 16:30:42 -0700

    Add GoPackagesFromReader.

--- exp/14/main.go
+++ exp/14/main.go
@@ -1,7 +1,10 @@
 package exp14
 
 import (
+	"io"
+
 	. "github.com/shurcooL/go/gists/gist7480523"
+	"github.com/shurcooL/go/gists/gist7651991"
 	. "github.com/shurcooL/go/gists/gist7802150"
 
 	"github.com/shurcooL/go/gists/gist8018045"
@@ -13,6 +16,7 @@ type GoPackageList interface {
 	DepNode2I
 }
 
+// GoPackages is a cached list of all Go packages in GOPATH including/excluding GOROOT.
 type GoPackages struct {
 	SkipGoroot bool // Currently, works on initial run only; changing its value afterwards has no effect.
 
@@ -46,5 +50,38 @@ func (this *GoPackages) Update() {
 }
 
 func (this *GoPackages) List() []*GoPackage {
+	return this.Entries
+}
+
+// GoPackagesFromReader is a cached list of Go packages specified by newline separated import paths from Reader.
+type GoPackagesFromReader struct {
+	Reader io.Reader
+
+	Entries []*GoPackage
+
+	DepNode2
+}
+
+func (this *GoPackagesFromReader) Update() {
+	reduceFunc := func(importPath string) interface{} {
+		if goPackage := GoPackageFromImportPath(importPath); goPackage != nil {
+			return goPackage
+		}
+		return nil
+	}
+
+	goPackages := gist7651991.GoReduceLinesFromReader(this.Reader, 8, reduceFunc)
+
+	this.Entries = nil
+	for {
+		if goPackage, ok := <-goPackages; ok {
+			this.Entries = append(this.Entries, goPackage.(*GoPackage))
+		} else {
+			break
+		}
+	}
+}
+
+func (this *GoPackagesFromReader) List() []*GoPackage {
 	return this.Entries
 }
