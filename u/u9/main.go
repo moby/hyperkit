5149be74d390171d3e00d7a490ac0481174cfafd
Dmitri Shuralyov <shurcooL@gmail.com>
2014-09-27 16:41:39 -0700

    Add AddTabSupport utility.
    It modifies a <textarea> so that pressing tab key will insert tabs.

---
+++ u/u9/main.go
@@ -0,0 +1,25 @@
+package u9
+
+import (
+	"github.com/gopherjs/gopherjs/js"
+	"honnef.co/go/js/dom"
+)
+
+// AddTabSupport is a helper that modifies a <textarea>, so that pressing tab key will insert tabs.
+func AddTabSupport(textArea *dom.HTMLTextAreaElement) {
+	textArea.AddEventListener("keydown", false, func(event dom.Event) {
+		switch ke := event.(*dom.KeyboardEvent); ke.KeyIdentifier {
+		case "U+0009": // Tab.
+			value, start, end := textArea.Value, textArea.SelectionStart, textArea.SelectionEnd
+
+			textArea.Value = value[:start] + "\t" + value[end:]
+
+			textArea.SelectionStart, textArea.SelectionEnd = start+1, start+1
+
+			event.PreventDefault()
+
+			inputEvent := js.Global.Get("CustomEvent").New("input")
+			textArea.Underlying().Call("dispatchEvent", inputEvent)
+		}
+	})
+}
