16a4e8f8845f0d6fd857c7cd3c88db9834b6fa64
Dmitri Shuralyov <shurcooL@gmail.com>
2014-10-03 20:14:36 -0700

    Avoid using dot imports.

--- exp/14/main.go
+++ exp/14/main.go
@@ -3,26 +3,25 @@ package exp14
 import (
 	"io"
 
-	. "github.com/shurcooL/go/gists/gist7480523"
+	"github.com/shurcooL/go/gists/gist7480523"
 	"github.com/shurcooL/go/gists/gist7651991"
-	. "github.com/shurcooL/go/gists/gist7802150"
-
+	"github.com/shurcooL/go/gists/gist7802150"
 	"github.com/shurcooL/go/gists/gist8018045"
 )
 
 type GoPackageList interface {
-	List() []*GoPackage
+	List() []*gist7480523.GoPackage
 
-	DepNode2I
+	gist7802150.DepNode2I
 }
 
 // GoPackages is a cached list of all Go packages in GOPATH including/excluding GOROOT.
 type GoPackages struct {
 	SkipGoroot bool // Currently, works on initial run only; changing its value afterwards has no effect.
 
-	Entries []*GoPackage
+	Entries []*gist7480523.GoPackage
 
-	DepNode2
+	gist7802150.DepNode2
 }
 
 func (this *GoPackages) Update() {
@@ -30,7 +29,7 @@ func (this *GoPackages) Update() {
 
 	// TODO: Make it load in background, without blocking, etc.
 	{
-		goPackages := make(chan *GoPackage, 64)
+		goPackages := make(chan *gist7480523.GoPackage, 64)
 
 		if this.SkipGoroot {
 			go gist8018045.GetGopathGoPackages(goPackages)
@@ -49,7 +48,7 @@ func (this *GoPackages) Update() {
 	}
 }
 
-func (this *GoPackages) List() []*GoPackage {
+func (this *GoPackages) List() []*gist7480523.GoPackage {
 	return this.Entries
 }
 
@@ -57,14 +56,14 @@ func (this *GoPackages) List() []*GoPackage {
 type GoPackagesFromReader struct {
 	Reader io.Reader
 
-	Entries []*GoPackage
+	Entries []*gist7480523.GoPackage
 
-	DepNode2
+	gist7802150.DepNode2
 }
 
 func (this *GoPackagesFromReader) Update() {
 	reduceFunc := func(importPath string) interface{} {
-		if goPackage := GoPackageFromImportPath(importPath); goPackage != nil {
+		if goPackage := gist7480523.GoPackageFromImportPath(importPath); goPackage != nil {
 			return goPackage
 		}
 		return nil
@@ -75,13 +74,13 @@ func (this *GoPackagesFromReader) Update() {
 	this.Entries = nil
 	for {
 		if goPackage, ok := <-goPackages; ok {
-			this.Entries = append(this.Entries, goPackage.(*GoPackage))
+			this.Entries = append(this.Entries, goPackage.(*gist7480523.GoPackage))
 		} else {
 			break
 		}
 	}
 }
 
-func (this *GoPackagesFromReader) List() []*GoPackage {
+func (this *GoPackagesFromReader) List() []*gist7480523.GoPackage {
 	return this.Entries
 }
