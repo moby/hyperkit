249c6af127a7b7e19b452855de176519968fbbbc
Dmitri Shuralyov <shurcooL@gmail.com>
2014-11-23 22:27:33 -0800

    sanitized_anchor_name: Squash non-alphanumeric characters into a single dash.
    Add tests.
    Closes #11.

--- github_flavored_markdown/sanitized_anchor_name/main.go
+++ github_flavored_markdown/sanitized_anchor_name/main.go
@@ -6,12 +6,17 @@ import "unicode"
 // Create returns a sanitized anchor name for the given text.
 func Create(text string) string {
 	var anchorName []rune
+	var futureDash = false
 	for _, r := range []rune(text) {
 		switch {
-		case r == ' ':
-			anchorName = append(anchorName, '-')
 		case unicode.IsLetter(r) || unicode.IsNumber(r):
+			if futureDash && len(anchorName) > 0 {
+				anchorName = append(anchorName, '-')
+			}
+			futureDash = false
 			anchorName = append(anchorName, unicode.ToLower(r))
+		default:
+			futureDash = true
 		}
 	}
 	return string(anchorName)
