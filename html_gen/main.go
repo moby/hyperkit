7550268269b51f163c4935cb334b6a7aa7983faa
Dmitri Shuralyov <shurcooL@gmail.com>
2014-11-26 20:49:07 -0800

    Add html_gen helper package for generating and rendering HTML nodes.

---
+++ html_gen/main.go
@@ -0,0 +1,38 @@
+// Package html_gen contains helper funcs for generating HTML nodes and rendering them.
+package html_gen
+
+import (
+	"bytes"
+	"html/template"
+
+	"golang.org/x/net/html"
+)
+
+// Text returns a plain text node.
+func Text(s string) *html.Node {
+	return &html.Node{
+		Type: html.TextNode, Data: s,
+	}
+}
+
+// A returns an anchor element <a href="{{.href}}">{{.s}}</a>.
+func A(s string, href template.URL) *html.Node {
+	return &html.Node{
+		Type: html.ElementNode, Data: "a",
+		Attr:       []html.Attribute{{Key: "href", Val: string(href)}},
+		FirstChild: Text(s),
+	}
+}
+
+// RenderNodes renders a list of HTML nodes.
+func RenderNodes(nodes ...*html.Node) (template.HTML, error) {
+	var buf bytes.Buffer
+	for _, node := range nodes {
+		err := html.Render(&buf, node)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return template.HTML(buf.String()), nil
+}
