241f8379347845280c5ae1d6d8947103ea9abe28
Dmitri Shuralyov <shurcooL@gmail.com>
2014-12-27 23:11:48 -0800

    Add GetAllIps to get a list of all IPv4 interfaces, including loopback.

--- gists/gist8065433/main.go
+++ gists/gist8065433/main.go
@@ -32,3 +32,34 @@ func GetPublicIps() (publicIps []string, err error) {
 
 	return publicIps, nil
 }
+
+// GetAllIps returns a string slice of all IPs.
+func GetAllIps() (ips []string, err error) {
+	ifis, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, ifi := range ifis {
+		addrs, err := ifi.Addrs()
+		if err != nil {
+			return nil, err
+		}
+
+		for _, addr := range addrs {
+			ipNet, ok := addr.(*net.IPNet)
+			if !ok {
+				continue
+			}
+
+			ip4 := ipNet.IP.To4()
+			if ip4 == nil {
+				continue
+			}
+
+			ips = append(ips, ipNet.IP.String())
+		}
+	}
+
+	return ips, nil
+}
