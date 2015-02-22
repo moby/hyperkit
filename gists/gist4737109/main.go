48ddf91826aea41d0fdb79bf801119785f6be690
Dmitri Shuralyov <shurcooL@gmail.com>
2015-02-22 00:29:50 -0800

    Delete unused commented-out code.
    Remove GistIdToGistContents and GistIdCommitIdToGistContents. This
    functionality is now available in github.com/google/go-github/github
    package.

--- gists/gist4737109/main.go
+++ gists/gist4737109/main.go
@@ -8,33 +8,6 @@ import (
 	"net/http"
 )
 
-/* TODO: Probably want to change interface to return error, etc. But it's worth doing when these funcs are used, not sooner.
-// This assumes there's only one file in the gist
-func GistIdToGistContents(gistId string) string {
-	return GistIdCommitIdToGistContents(gistId, "")
-}
-
-// This assumes there's only one file in the gist
-func GistIdCommitIdToGistContents(gistId, commitId string) string {
-	gistUrl := "https://api.github.com/gists/" + gistId
-	if "" != commitId {
-		gistUrl += "/" + commitId
-	}
-
-	var gistJson struct {
-		Files map[string]struct{ Content string }
-		//History []struct{ Version string }
-	}
-	err := json.Unmarshal(httpGetB(gistUrl), &gistJson)
-	if err != nil {
-		fmt.Println("error:", err)
-	}
-	for _, v := range gistJson.Files {
-		return v.Content
-	}
-	return ""
-}*/
-
 func GistIdToUsername(gistId string) (string, error) {
 	gistUrl := "https://api.github.com/gists/" + gistId
 
@@ -50,11 +23,8 @@ func GistIdToUsername(gistId string) (string, error) {
 
 func main() {
 	gistId := "4737109"
-	//commitId := "1b4e4b0e6f469d5e5a91b49028fbf2ab936bfcd4"
 
 	fmt.Println(GistIdToUsername(gistId))
-	//println(GistIdCommitIdToGistContents(gistId, commitId))
-	//println(GistIdToGistContents(gistId))
 }
 
 // ---
