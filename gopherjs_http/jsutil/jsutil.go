da2b142071b4799a8ab0521cb17604819946af2a
Dmitri Shuralyov <shurcooL@gmail.com>
2015-02-22 01:50:56 -0800

    Port to new js.Object struct.

--- gopherjs_http/jsutil/jsutil.go
+++ gopherjs_http/jsutil/jsutil.go
@@ -9,20 +9,20 @@ import (
 	"honnef.co/go/js/dom"
 )
 
-// Wrap returns a wrapper func that handles the conversion from native JavaScript js.Object parameters
+// Wrap returns a wrapper func that handles the conversion from native JavaScript *js.Object parameters
 // to the following types.
 //
-// It supports js.Object (left unmodified), dom.Document, dom.Element, dom.Event, dom.HTMLElement, dom.Node.
+// It supports *js.Object (left unmodified), dom.Document, dom.Element, dom.Event, dom.HTMLElement, dom.Node.
 //
 // For other types, the input is assumed to be a JSON string which is then unmarshalled into that type.
-func Wrap(fn interface{}) func(...js.Object) {
+func Wrap(fn interface{}) func(...*js.Object) {
 	v := reflect.ValueOf(fn)
-	return func(args ...js.Object) {
+	return func(args ...*js.Object) {
 		in := make([]reflect.Value, v.Type().NumIn())
 		for i := range in {
 			switch t := v.Type().In(i); t {
-			// js.Object is passed through.
-			case typeOf((*js.Object)(nil)):
+			// *js.Object is passed through.
+			case typeOf((**js.Object)(nil)):
 				in[i] = reflect.ValueOf(args[i])
 
 			// dom types are wrapped.
