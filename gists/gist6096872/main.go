4ef562f157ea1d3833dc835dc9da8c55f45de032
Dmitri Shuralyov <shurcooL@gmail.com>
2015-04-18 22:54:57 -0700

    Remove unused ByteReader.

--- gists/gist6096872/main.go
+++ gists/gist6096872/main.go
@@ -8,46 +8,15 @@ import (
 type ChanWriter chan []byte
 
 func (cw ChanWriter) Write(p []byte) (n int, err error) {
-	// TODO: Copy the slice contents rather than sending the original, as it may get modified?
+	// TODO: Copy the slice contents rather than sending the original, as it may get modified after we return?
 	cw <- p
 	return len(p), nil
-}
-
-// Credit to Tarmigan.
-// TODO: Delete this? It's not used anywhere.
-func ByteReader(r io.Reader) <-chan []byte {
-	ch := make(chan []byte)
-	go func() {
-		for {
-			buf := make([]byte, 2048)
-			s := 0
-
-		Inner:
-			for {
-				n, err := r.Read(buf[s:])
-				if n > 0 {
-					ch <- buf[s : s+n]
-					s += n
-				}
-				if err != nil {
-					close(ch)
-					return
-				}
-				if s >= len(buf) {
-					break Inner
-				}
-			}
-		}
-	}()
-
-	return ch
 }
 
 func LineReader(r io.Reader) <-chan []byte {
 	ch := make(chan []byte)
 	go func() {
 		br := bufio.NewReader(r)
-
 		for {
 			line, err := br.ReadBytes('\n')
 			if err != nil {
@@ -55,10 +24,8 @@ func LineReader(r io.Reader) <-chan []byte {
 				close(ch)
 				return
 			}
-
 			ch <- line[:len(line)-1] // Trim last newline.
 		}
 	}()
-
 	return ch
 }
