2b8f6a6ea89c8d221bca8ed092f7a25b36149c40
Dmitri Shuralyov <shurcooL@gmail.com>
2015-06-02 01:53:20 -0700

    time_util: Improve docs to match Go style.

--- time_util/main.go
+++ time_util/main.go
@@ -2,7 +2,7 @@ package time_util
 
 import "time"
 
-// Returns time at start of week of t, in local timezone.
+// StartOfWeek returns time at start of week of t, in local timezone.
 func StartOfWeek(t time.Time) time.Time {
 	return time.Date(t.Year(), t.Month(), t.Day()-int(t.Weekday()), 0, 0, 0, 0, time.Local)
 }
