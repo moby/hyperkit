e0269a3a78f8fa9c00aeeb7abf95560928b46e08
Dmitri Shuralyov <shurcooL@gmail.com>
2015-07-31 18:09:57 -0700

    trim: Minor refactor.

--- trim/main.go
+++ trim/main.go
@@ -3,7 +3,7 @@ package trim
 
 // LastNewline trims the last newline character from str, if any.
 func LastNewline(str string) string {
-	if len(str) >= 1 && '\n' == str[len(str)-1] {
+	if len(str) >= 1 && str[len(str)-1] == '\n' {
 		return str[:len(str)-1]
 	} else {
 		return str
@@ -12,7 +12,7 @@ func LastNewline(str string) string {
 
 // FirstSpace trims the first space character from str, if any.
 func FirstSpace(str string) string {
-	if len(str) >= 1 && ' ' == str[0] {
+	if len(str) >= 1 && str[0] == ' ' {
 		return str[1:]
 	} else {
 		return str
