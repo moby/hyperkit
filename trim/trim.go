bf2bec17bc2e816c7fbc32a05ac978b8fb6f3ffd
Dmitri Shuralyov <shurcooL@gmail.com>
2015-12-26 21:26:24 -0800

    trim: Refactor to use better style.
    Also improve test coverage.

--- trim/trim.go
+++ trim/trim.go
@@ -3,18 +3,16 @@ package trim
 
 // LastNewline trims the last newline character from str, if any.
 func LastNewline(str string) string {
-	if len(str) >= 1 && str[len(str)-1] == '\n' {
-		return str[:len(str)-1]
-	} else {
+	if len(str) < 1 || str[len(str)-1] != '\n' {
 		return str
 	}
+	return str[:len(str)-1]
 }
 
 // FirstSpace trims the first space character from str, if any.
 func FirstSpace(str string) string {
-	if len(str) >= 1 && str[0] == ' ' {
-		return str[1:]
-	} else {
+	if len(str) < 1 || str[0] != ' ' {
 		return str
 	}
+	return str[1:]
 }
