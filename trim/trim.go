a4888f33d66749254f993eb18e1bc1497abc5927
Dmitri Shuralyov <shurcooL@gmail.com>
2015-12-26 21:57:58 -0800

    trim: Use a simpler variable name.
    This is shorter, and more similar to strings package.

--- trim/trim.go
+++ trim/trim.go
@@ -1,18 +1,18 @@
 // Package trim contains helpers for trimming strings.
 package trim
 
-// LastNewline trims the last newline character from str, if any.
-func LastNewline(str string) string {
-	if len(str) < 1 || str[len(str)-1] != '\n' {
-		return str
+// LastNewline trims the last newline character from s, if any.
+func LastNewline(s string) string {
+	if len(s) < 1 || s[len(s)-1] != '\n' {
+		return s
 	}
-	return str[:len(str)-1]
+	return s[:len(s)-1]
 }
 
-// FirstSpace trims the first space character from str, if any.
-func FirstSpace(str string) string {
-	if len(str) < 1 || str[0] != ' ' {
-		return str
+// FirstSpace trims the first space character from s, if any.
+func FirstSpace(s string) string {
+	if len(s) < 1 || s[0] != ' ' {
+		return s
 	}
-	return str[1:]
+	return s[1:]
 }
--- trim/trim_test.go
+++ trim/trim_test.go
@@ -11,14 +11,14 @@ func ExampleLastNewline() {
 	fmt.Printf("%q\n", trim.LastNewline("String"))
 	fmt.Printf("%q\n", trim.LastNewline("\n"))
 	fmt.Printf("%q\n", trim.LastNewline(""))
-	fmt.Printf("%q\n", trim.LastNewline("String\n\n"))
+	fmt.Printf("%q\n", trim.LastNewline("  String\n\n"))
 
 	// Output:
 	// "String"
 	// "String"
 	// ""
 	// ""
-	// "String\n"
+	// "  String\n"
 }
 
 func ExampleFirstSpace() {
@@ -26,12 +26,12 @@ func ExampleFirstSpace() {
 	fmt.Printf("%q\n", trim.FirstSpace("String"))
 	fmt.Printf("%q\n", trim.FirstSpace(" "))
 	fmt.Printf("%q\n", trim.FirstSpace(""))
-	fmt.Printf("%q\n", trim.FirstSpace("  String"))
+	fmt.Printf("%q\n", trim.FirstSpace("  String\n\n"))
 
 	// Output:
 	// "String"
 	// "String"
 	// ""
 	// ""
-	// " String"
+	// " String\n\n"
 }
