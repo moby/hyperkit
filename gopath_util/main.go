d7c647097623760f8b4b2aff4a581df10fb998f5
Dmitri Shuralyov <shurcooL@gmail.com>
2015-12-27 21:27:14 -0800

    gopath_util: Stop importing deprecated package.

--- gopath_util/main.go
+++ gopath_util/main.go
@@ -8,7 +8,6 @@ import (
 	"github.com/kisielk/gotool"
 	"github.com/shurcooL/go/gists/gist7480523"
 	"github.com/shurcooL/go/trash"
-	"github.com/shurcooL/gostatus/status"
 )
 
 // RemoveRepo removes go-gettable repo with no local changes (by moving it into trash).
@@ -54,16 +53,64 @@ func RemoveRepo(importPathPattern string) error {
 	firstGoPackage.UpdateVcsFields()
 
 	cleanStatus := func(goPackage *gist7480523.GoPackage) bool {
-		packageStatus := status.PlumbingPresenterV2(goPackage)[:4]
+		packageStatus := presenter(goPackage)[:4]
 		return packageStatus == "    " || packageStatus == "  + " // Updates are okay to ignore.
 	}
 
 	if !cleanStatus(firstGoPackage) {
-		return errors.New("non-clean status: " + status.PorcelainPresenter(firstGoPackage))
+		return errors.New("non-clean status: " + presenter(firstGoPackage))
 	}
 
 	err := trash.MoveTo(firstGoPackage.Dir.Repo.Vcs.RootPath())
 	return err
 
 	// TODO: Clean up /pkg folder contents, if any, etc.
+}
+
+// TODO: Inline and simplify this.
+var presenter gist7480523.GoPackageStringer = func(goPackage *gist7480523.GoPackage) string {
+	out := ""
+
+	if repo := goPackage.Dir.Repo; repo != nil {
+		repoImportPath := gist7480523.GetRepoImportPath(repo.Vcs.RootPath(), goPackage.Bpkg.SrcRoot)
+
+		if repo.VcsLocal.LocalBranch != repo.Vcs.GetDefaultBranch() {
+			out += "b"
+		} else {
+			out += " "
+		}
+		if repo.VcsLocal.Status != "" {
+			out += "*"
+		} else {
+			out += " "
+		}
+		if repo.RepoRoot == nil || repo.RepoRoot.Repo != repo.VcsLocal.Remote {
+			out += "#"
+		} else if repo.VcsLocal.LocalRev != repo.VcsRemote.RemoteRev {
+			if repo.VcsRemote.RemoteRev != "" {
+				if !repo.VcsRemote.IsContained {
+					out += "+"
+				} else {
+					out += "-"
+				}
+			} else {
+				out += "!"
+			}
+		} else {
+			out += " "
+		}
+		if repo.VcsLocal.Stash != "" {
+			out += "$"
+		} else {
+			out += " "
+		}
+
+		out += " " + repoImportPath + "/..."
+	} else {
+		out += "????"
+
+		out += " " + goPackage.Bpkg.ImportPath
+	}
+
+	return out
 }
