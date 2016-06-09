8b8a7a02e1cd1779f688fa216007012e3d9587a9
Dmitri Shuralyov <shurcooL@gmail.com>
2016-06-08 23:18:23 -0700

    ospath: Add support for Linux, 386/amd64.
    Use $HOME/.cache directory for cache files. This is based on based on
    https://github.com/rsc/gt/blob/ee152ddc9ec2a99d345cf81b6ba0f40a83c9dd6f/main.go#L22.

--- ospath/ospath.go
+++ ospath/ospath.go
@@ -26,12 +26,19 @@ func CacheDir(importPath string) (string, error) {
 	} else {
 		home = u.HomeDir
 	}
-	// TODO: Support Linux, Windows in analogous ways. Also support mobile devices (iOS, Android).
+	// TODO: Support Windows in analogous ways. Also support mobile devices (iOS, Android).
 	//       Think about web? While HTML5 Local Storage could be used, it's not going to be compatible
 	//       with filepaths; so maybe consider returning a webdav.FileSystem or so instead? Needs consideration.
 	switch {
 	case runtime.GOOS == "darwin" && runtime.GOARCH == "amd64":
 		dir := filepath.Join(home, "Library", "Caches", filepath.FromSlash(importPath))
+		if err := os.MkdirAll(dir, 0700); err != nil {
+			return "", err
+		}
+		return dir, nil
+	case runtime.GOOS == "linux" && (runtime.GOARCH == "386" || runtime.GOARCH == "amd64"):
+		// $HOME/.cache path is based on https://github.com/rsc/gt/blob/ee152ddc9ec2a99d345cf81b6ba0f40a83c9dd6f/main.go#L22.
+		dir := filepath.Join(home, ".cache", filepath.FromSlash(importPath))
 		if err := os.MkdirAll(dir, 0700); err != nil {
 			return "", err
 		}
