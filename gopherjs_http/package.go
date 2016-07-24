28c7d33abae2d028e9adb5abd1c3600d2986d813
Dmitri Shuralyov <shurcooL@gmail.com>
2016-07-24 13:40:41 -0400

    gopherjs_http: Simpler method receiver name.

--- gopherjs_http/package.go
+++ gopherjs_http/package.go
@@ -9,20 +9,20 @@ import (
 // Package returns an http.FileSystem that contains a single file at root,
 // containing result of building package with importPath using GopherJS.
 func Package(importPath string) http.FileSystem {
-	return &packageFS{importPath: importPath}
+	return packageFS{importPath: importPath}
 }
 
 type packageFS struct {
 	importPath string
 }
 
-func (pfs packageFS) Open(name string) (http.File, error) {
+func (fs packageFS) Open(name string) (http.File, error) {
 	if name != "/" {
 		return nil, &os.PathError{Op: "open", Path: name, Err: os.ErrNotExist}
 	}
-	p, err := build.Import(pfs.importPath, "", build.FindOnly)
+	p, err := build.Import(fs.importPath, "", build.FindOnly)
 	if err != nil {
-		return nil, &os.PathError{Op: `"go/build".Import`, Path: pfs.importPath, Err: err}
+		return nil, &os.PathError{Op: `"go/build".Import`, Path: fs.importPath, Err: err}
 	}
 	return (&gopherJSFS{source: http.Dir(p.SrcRoot)}).compileGoPackage(p.ImportPath)
 }
