2e2ad599809f50f10d776fff3d723d4ce9fdb5e0
David Sheets <dsheets@docker.com>
2016-12-04 16:03:08 +0000

    dtrace/eptfault.d: use spaces instead of tabs
    Signed-off-by: David Sheets <dsheets@docker.com>

--- dtrace/eptfault.d
+++ dtrace/eptfault.d
@@ -9,16 +9,16 @@
 
 dtrace:::BEGIN
 {
-	printf("Tracing... Hit Ctrl-C to end.\n");
+        printf("Tracing... Hit Ctrl-C to end.\n");
 }
 
 hyperkit$target:::vmx-ept-fault
 {
-	@num[arg1, arg0] = count();
+        @num[arg1, arg0] = count();
 }
 
 dtrace:::END
 {
-	printf("%18s %-4s %8s\n", "ADDRESS", "vCPU", "COUNT");
-	printa("%18x %-4d %@8d\n", @num);
+        printf("%18s %-4s %8s\n", "ADDRESS", "vCPU", "COUNT");
+        printa("%18x %-4d %@8d\n", @num);
 }
