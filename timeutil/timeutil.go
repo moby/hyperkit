fdc15b12f4dbaa0f1adb34d2a0b3fddd275c957b
Dmitri Shuralyov <shurcooL@gmail.com>
2016-12-15 14:52:26 -0500

    timeutil: Add StartOfDay to get start of day.

--- timeutil/timeutil.go
+++ timeutil/timeutil.go
@@ -3,6 +3,11 @@ package timeutil
 
 import "time"
 
+// StartOfDay returns time at start of day of t.
+func StartOfDay(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+}
+
 // StartOfWeek returns time at start of week of t.
 func StartOfWeek(t time.Time) time.Time {
 	return time.Date(t.Year(), t.Month(), t.Day()-int(t.Weekday()), 0, 0, 0, 0, t.Location())
