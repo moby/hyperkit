336fbb825bab762338de2255c0f09d86e1bd6912
Akim Demaille <akim.demaille@docker.com>
2018-04-03 17:22:01 +0200

    Asl: prefer a static buffer
    Suggested by Rolf Neugebauer.
    
    Signed-off-by: Akim Demaille <akim.demaille@docker.com>

--- src/lib/asl.c
+++ src/lib/asl.c
@@ -11,45 +11,32 @@
 static aslclient asl = NULL;
 static aslmsg log_msg = NULL;
 
-static unsigned char *buf = NULL;
+static unsigned char buf[4096];
 static size_t buf_size = 0;
-static size_t buf_capacity = 0;
 
 /* asl is deprecated in favor of os_log starting with macOS 10.12.  */
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-
-/* Grow buf/buf_capacity. */
-static void buf_grow(void)
-{
-	buf_capacity = buf_capacity ? 2 * buf_capacity : 1024;
-	buf = realloc(buf, buf_capacity);
-	if (!buf) {
-		perror("buf_grow");
-		exit(1);
-	}
-}
-
 
 /* Initialize ASL logger and local buffer. */
 void asl_init(void)
 {
 	asl = asl_open(NULL, NULL, 0);
 	log_msg = asl_new(ASL_TYPE_MSG);
-	buf_grow();
 }
 
 
 /* Send one character to the logger: wait for full lines before actually sending. */
 void asl_put(uint8_t c)
 {
-	if (buf_size + 1 >= buf_capacity) {
-		buf_grow();
-	}
 	if (c == '\n') {
 		buf[buf_size] = 0;
 		asl_log(asl, log_msg, ASL_LEVEL_NOTICE, "%s", buf);
 		buf_size = 0;
 	} else {
+		if (buf_size + 2 >= sizeof buf) {
+			/* Running out of space, flush.  */
+			asl_put('\n');
+		}
 		buf[buf_size] = c;
 		++buf_size;
 	}
