797e17b40a1952b50c79a3beac8b6a4ad0e17fd2
Dmitri Shuralyov <dmitri@shuralyov.com>
2019-12-16 00:10:21 -0500

    gopherjs_http/jsutil: add v2 version for dom/v2
    This is needed to be able to support WebAssembly
    in addition to GopherJS.

--- gopherjs_http/jsutil/v2/jsutil.go
+++ gopherjs_http/jsutil/v2/jsutil.go
@@ -1,21 +1,21 @@
 // Package jsutil provides utility functions for interacting with
-// native JavaScript APIs via github.com/gopherjs/gopherjs/js API.
-// It has support for common types in honnef.co/go/js/dom.
+// native JavaScript APIs via syscall/js API.
+// It has support for common types in honnef.co/go/js/dom/v2.
 package jsutil
 
 import (
 	"encoding/json"
 	"fmt"
 	"reflect"
+	"syscall/js"
 
-	"github.com/gopherjs/gopherjs/js"
-	"honnef.co/go/js/dom"
+	"honnef.co/go/js/dom/v2"
 )
 
-// Wrap returns a wrapper func that handles the conversion from native JavaScript *js.Object parameters
+// Wrap returns a wrapper func that handles the conversion from native JavaScript js.Value parameters
 // to the following types.
 //
-// It supports *js.Object (left unmodified), dom.Document, dom.Element, dom.Event, dom.HTMLElement, dom.Node.
+// It supports js.Value (left unmodified), dom.Document, dom.Element, dom.Event, dom.HTMLElement, dom.Node.
 // It has to be one of those types exactly; it can't be another type that implements the interface like *dom.BasicElement.
 //
 // For other types, the input is assumed to be a JSON string which is then unmarshalled into that type.
@@ -32,19 +32,19 @@ import (
 // 	}
 //
 // 	func main() {
-// 		js.Global.Set("Handler", jsutil.Wrap(Handler))
+// 		js.Global().Set("Handler", jsutil.Wrap(Handler))
 // 	}
-func Wrap(fn interface{}) func(...*js.Object) {
+func Wrap(fn interface{}) js.Func {
 	v := reflect.ValueOf(fn)
-	return func(args ...*js.Object) {
+	return js.FuncOf(func(_ js.Value, args []js.Value) interface{} {
 		if len(args) != v.Type().NumIn() {
 			panic(fmt.Errorf("wrapped %v got %v arguments, want %v", v.Type().String(), len(args), v.Type().NumIn()))
 		}
 		in := make([]reflect.Value, v.Type().NumIn())
 		for i := range in {
 			switch t := v.Type().In(i); t {
-			// *js.Object is passed through.
-			case typeOf((**js.Object)(nil)):
+			// js.Value is passed through.
+			case typeOf((*js.Value)(nil)):
 				in[i] = reflect.ValueOf(args[i])
 
 			// dom types are wrapped.
@@ -61,6 +61,9 @@ func Wrap(fn interface{}) func(...*js.Object) {
 
 			// Unmarshal incoming encoded JSON into the Go type.
 			default:
+				if args[i].Type() != js.TypeString {
+					panic(fmt.Errorf("jsutil: incoming value type is %s; want a string with JSON content", args[i].Type()))
+				}
 				p := reflect.New(t)
 				err := json.Unmarshal([]byte(args[i].String()), p.Interface())
 				if err != nil {
@@ -70,7 +73,8 @@ func Wrap(fn interface{}) func(...*js.Object) {
 			}
 		}
 		v.Call(in)
-	}
+		return nil
+	})
 }
 
 // typeOf returns the reflect.Type of what the pointer points to.
