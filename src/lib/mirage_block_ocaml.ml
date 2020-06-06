6414a3c33383c011044ba1df8f6fd4965a52118b
David Scott <dave@recoil.org>
2020-06-06 16:21:10 +0100

    ocaml: ensure the notifications fd is set to non-blocking
    Since some users have observed spinning, it could be that the system
    thinks the fd should be blocking but it is actually non-blocking and
    returning immediately.
    
    Signed-off-by: David Scott <dave@recoil.org>

--- src/lib/mirage_block_ocaml.ml
+++ src/lib/mirage_block_ocaml.ml
@@ -416,9 +416,13 @@ let process_one t =
    requests and forks background threads to process all the requests. *)
 let serve_forever () =
   let buf = Bytes.make 1 '\000' in
-  let request_reader = Lwt_unix.of_unix_file_descr Protocol.request_reader in
+  (* According to https://ocsigen.org/lwt/3.2.1/api/Lwt_unix non-blocking mode
+     is fastest, and used by default with Unix pipes. *)
+  let blocking = false in
+  let request_reader = Lwt_unix.of_unix_file_descr ~blocking Protocol.request_reader in
 
   let rec loop () =
+    Lwt_unix.set_blocking request_reader blocking;
     Lwt_unix.read request_reader buf 0 1
     >>= fun n ->
     if n = 0 then begin
