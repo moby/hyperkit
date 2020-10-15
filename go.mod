7c4e0eccafdcf9eecb29d4c130cf1a60870cb5e6
Priya Wadhwa <priyawadhwa@google.com>
2020-10-14 18:28:48 -0700

    Use unix.IoctlSetTermios instead of ioctl system call on darwin
    This is necessary due to an upgrade in Golang's sys package, which no longer allows for direct system calls on darwin.
    
    Signed-off-by: Priya Wadhwa <priyawadhwa@google.com>

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.14
 require (
 	github.com/mitchellh/go-ps v1.0.0
 	github.com/stretchr/testify v1.6.1
-	golang.org/x/sys v0.0.0-20200722175500-76b94024e4b6
+	golang.org/x/sys v0.0.0-20201015000850-e3ed0017c211
 )
--- go/pty_util_darwin.go
+++ go/pty_util_darwin.go
@@ -7,24 +7,21 @@ which is under Apache License Version 2.0, January 2004
 */
 import (
 	"os"
-	"unsafe"
 
 	"golang.org/x/sys/unix"
 )
 
 func tcget(fd uintptr, p *unix.Termios) error {
-	return ioctl(fd, unix.TIOCGETA, uintptr(unsafe.Pointer(p)))
+	termios, err := unix.IoctlGetTermios(int(fd), unix.TIOCGETA)
+	if err != nil {
+		return err
+	}
+	*p = *termios
+	return nil
 }
 
 func tcset(fd uintptr, p *unix.Termios) error {
-	return ioctl(fd, unix.TIOCSETA, uintptr(unsafe.Pointer(p)))
-}
-
-func ioctl(fd, flag, data uintptr) error {
-	if _, _, err := unix.Syscall(unix.SYS_IOCTL, fd, flag, data); err != 0 {
-		return err
-	}
-	return nil
+	return unix.IoctlSetTermios(int(fd), unix.TIOCSETA, p)
 }
 
 func saneTerminal(f *os.File) error {
